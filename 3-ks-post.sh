17bae3b5bfab7b7492bc2bfde0c1c7f0a068641f
tabowling <terry.bowling@gmail.com>
2016-08-16 08:40:06 -0400

    updates

--- 3-ks-post.sh
+++ 3-ks-post.sh
@@ -19,10 +19,15 @@ _EOF_
 # hypervisor hosts file
 echo "$MYHOSTS" >> /etc/hosts
 
+PUBRSA=`cat /root/.ssh/id_rsa.pub`
+
 # add to kickstart %post section
 cat >> $KS << _EOF_
-PUBRSA=`cat /root/.ssh/id_rsa.pub`
+
+%post --log=/root/ks-post.log
+/usr/bin/ssh-keygen  -qt rsa -N "" -f /root/.ssh/id_rsa
 echo "$PUBRSA" >> /root/.ssh/authorized_keys 
+
 echo "192.168.$OCTET.200  $SATSERV.example$OCTET.com $SATSERV" >> /etc/hosts
 echo "192.168.$OCTET.101  client1.example$OCTET.com" >> /etc/hosts
 echo "192.168.$OCTET.102  client2.example$OCTET.com" >> /etc/hosts
@@ -33,6 +38,17 @@ echo "192.168.$OCTET.106  client6.example$OCTET.com" >> /etc/hosts
 echo "192.168.$OCTET.107  client7.example$OCTET.com" >> /etc/hosts
 echo "192.168.$OCTET.108  client8.example$OCTET.com" >> /etc/hosts
 echo "192.168.$OCTET.109  client9.example$OCTET.com" >> /etc/hosts
+
+/usr/sbin/subscription-manager register  --username=$RHNUSER --password $RHNPASS
+/usr/sbin/subscription-manager attach --pool=$RHNPOOL
+/usr/sbin/subscription-manager repos --disable=rh*
+/usr/sbin/subscription-manager repos  \
+        --enable=rhel-7-server-rpms \
+        --enable=rhel-7-server-eus-rpms \
+        --enable=rhel-7-server-extras-rpms \
+        --enable=rhel-7-server-optional-rpms 
+
+yum -y update 
 
 %end
 
