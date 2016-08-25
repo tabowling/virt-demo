9be59868a4a32553967e6e741bb82c7568d2c9ae
tabowling <terry.bowling@gmail.com>
2016-08-25 10:20:50 -0400

    added virsh net remove script

---
+++ 7-rm-virshnet.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+source 1-config.sh
+
+echo "Octet $OCTET"
+echo "$VIRSHNETNAME"
+
+#  Stop and delete the virt net
+# virsh net-list --all
+# virsh net-destroy ${VIRSHNETNAME}
+# virsh net-undefine ${VIRSHNETNAME}
+
