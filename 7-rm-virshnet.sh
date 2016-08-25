eab9088eeb361ab03fe5e04ab74f700b2ae6be52
tabowling <terry.bowling@gmail.com>
2016-08-25 10:23:58 -0400

    updates

--- 7-rm-virshnet.sh
+++ 7-rm-virshnet.sh
@@ -5,7 +5,8 @@ echo "Octet $OCTET"
 echo "$VIRSHNETNAME"
 
 #  Stop and delete the virt net
-# virsh net-list --all
-# virsh net-destroy ${VIRSHNETNAME}
-# virsh net-undefine ${VIRSHNETNAME}
+ virsh net-list --all
+ virsh net-destroy ${VIRSHNETNAME}
+ virsh net-undefine ${VIRSHNETNAME}
+ virsh net-list --all
 
