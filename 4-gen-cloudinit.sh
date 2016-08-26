39bfd0f711e4fec8b1d9411fa348e04e184ccbb3
tabowling <terry.bowling@gmail.com>
2016-08-26 07:13:18 -0400

    removed cloudinit rh_subscription and post config cuz cloud-init sucks.  ansible++

--- 4-gen-cloudinit.sh
+++ 4-gen-cloudinit.sh
@@ -12,20 +12,6 @@ fi
 # grab hypervisor root user's ssh key to insert
 PUBRSA=`cat ~/.ssh/id_rsa.pub`
 echo $PUBRSA
-echo $PWD
-FOREMAN_RSA=`cat ./foreman.id_rsa`
-FOREMAN_PUB=`cat ./foreman.id_rsa.pub`
-
-FOREMAN_KEY_SETUP=""
-if [ "$DOMNAME" == "sat6demo[1-9]" ]
-then 
-	# pregenerate foreman keys  /usr/bin/ssh-keygen -qt rsa -f ./foreman.id_rsa -C "foreman@${SATSERV}" -N ""
-	cat foreman.id_rsa.pub >> /root/.ssh/known_hosts;
-#	FOREMAN_RSA=`cat ./foreman.id_rsa`;
-#	FOREMAN_PUB=`cat ./foreman.id_rsa.pub`;
-	#FORMAN_KEY_SETUP="  - mkdir -p /usr/share/foreman/.ssh/;  - echo $FOREMAN_RSA > /usr/share/foreman/.ssh/id_rsa;  - echo $FOREMAN_PUB > /usr/share/foreman/.ssh/id_rsa.pub;";
-	#echo $FOREMAN_KEY_SETUP";
-fi
 
 
 # Create meta-data for cloud-init
@@ -57,38 +43,41 @@ chpasswd:
     root:redhat
     cloud-user:redhat
   expire: False
-rh_subscription:
-  username: $RHNUSER
-  password: $RHNPASS
-  add-pool:
-    - 8a85f9833e1404a9013e3cddf95a0599
-    - 8a85f981501430fe015019593a930646
-  disable-repo:
-    - '*'
-  enable-repo:
-    - rhel-7-server-rpms
-    - rhel-server-rhscl-7-rpms
-    - rhel-7-server-satellite-6.2-rpms
-runcmd:
-  - subscription-manager repos --disable=*
-  - subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-server-rhscl-7-rpms --enable=rhel-7-server-satellite-6.2-rpms
-  - yum -y install firewalld
-  - systemctl start firewalld.service
-  - systemctl enable firewalld.service
-  - yum -y install satellite
-  - su - foreman -s /bin/bash -c '/usr/bin/ssh-keygen -qt rsa -f /usr/share/foreman/.ssh/id_rsa -C "foreman@${SATSERV}" -N "" '
-  - su - foreman -s /bin/bash -c 'ssh-keyscan 192.168.11.1 >> /usr/share/foreman/.ssh/known_hosts'
-  - yum -y groupinstall "Server with GUI"
-  - systemctl set-default graphical.target
-  - 'firewall-cmd --permanent --add-service=RH-Satellite-6 --add-service=dns --add-service=dhcp --add-service=tftp --add-service=http --add-service=https '
-  - 'firewall-cmd --permanent --add-port="5674/tcp" '
-  - firewall-cmd --reload
 
 _EOF_
 
 #
 #  - echo "$FOREMAN_RSA" > /usr/share/foreman/.ssh/id_rsa
 #  - echo "$FOREMAN_PUB" > /usr/share/foreman/.ssh/id_rsa.pub
+
+
+#rh_subscription:
+#  username: $RHNUSER
+#  password: $RHNPASS
+#  add-pool:
+#    - 8a85f9833e1404a9013e3cddf95a0599
+#    - 8a85f981501430fe015019593a930646
+#  disable-repo:
+#    - '*'
+#  enable-repo:
+#    - rhel-7-server-rpms
+#    - rhel-server-rhscl-7-rpms
+#    - rhel-7-server-satellite-6.2-rpms
+#runcmd:
+#  - subscription-manager repos --disable=*
+#  - subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-server-rhscl-7-rpms --enable=rhel-7-server-satellite-6.2-rpms
+#  - yum -y install firewalld
+#  - systemctl start firewalld.service
+#  - systemctl enable firewalld.service
+#  - yum -y install satellite
+#  - su - foreman -s /bin/bash -c '/usr/bin/ssh-keygen -qt rsa -f /usr/share/foreman/.ssh/id_rsa -C "foreman@${SATSERV}" -N "" '
+#  - su - foreman -s /bin/bash -c 'ssh-keyscan 192.168.11.1 >> /usr/share/foreman/.ssh/known_hosts'
+#  - yum -y groupinstall "Server with GUI"
+#  - systemctl set-default graphical.target
+#  - 'firewall-cmd --permanent --add-service=RH-Satellite-6 --add-service=dns --add-service=dhcp --add-service=tftp --add-service=http --add-service=https '
+#  - 'firewall-cmd --permanent --add-port="5674/tcp" '
+#  - firewall-cmd --reload
+#
 
 # Create the image and move into place
 genisoimage -output ${DOMNAME}-cloudinit.iso -volid cidata -joliet -rock user-data meta-data
